f904dcac809ab5e922597ea6dcb2dec7437ebc48
cyrilf <cyril@reelyactive.com>
2013-05-08 14:49:17 -0400

    Add a response template object
    [Feature #49371377]
    
    - Add a response template for a common response object
      Contains the main errors we want to handle

---
+++ utils/responseTemplate.js
@@ -0,0 +1,110 @@
+var restify = require('restify');
+var util = require('util');
+
+var ResponseTemplate = {
+  // 200
+  // ok: function(message) {
+  //   restify.RestError.call(this, {
+  //     restCode: 'OK',
+  //     statusCode: 200,
+  //     message: message,
+  //     constructorOpt: ResponseTemplate.ok
+  //   });
+  //   delete this.body;
+  //   this.statusCode = 200;
+  //   this.developerMessage = 'ok';
+  //   this.userMessage = 'Everything is ok.';
+  //   this.errorCode = null;
+  //   this.moreInfo = 'Documentation not implemented yet';
+  // },
+
+  // 400
+  badRequest: function(message) {
+    restify.RestError.call(this, {
+      restCode: 'Bad Request',
+      statusCode: 400,
+      message: message,
+      constructorOpt: ResponseTemplate.badRequest
+    });
+    delete this.body;
+    this.statusCode = 400;
+    this.developerMessage = 'badRequest';
+    this.userMessage = 'An error occured due to a missing information.';
+    this.errorCode = 400;
+    this.moreInfo = 'Documentation not implemented yet';
+  },
+
+  // 404
+  notFound: function(message) {
+    restify.RestError.call(this, {
+      restCode: 'Not Found',
+      statusCode: 404,
+      message: message,
+      constructorOpt: ResponseTemplate.notFound
+    });
+    delete this.body;
+    this.statusCode = 404;
+    this.developerMessage = 'notFound';
+    this.userMessage = 'Ressource not found';
+    this.errorCode = 404;
+    this.moreInfo = 'Documentation not implemented yet';
+  },
+
+  // 406
+  notAcceptable: function(message) {
+    restify.RestError.call(this, {
+      restCode: 'Not Acceptable',
+      statusCode: 406,
+      message: message,
+      constructorOpt: ResponseTemplate.notAcceptable
+    });
+    delete this.body;
+    this.statusCode = 406;
+    this.developerMessage = 'notAcceptable';
+    this.userMessage = 'This action isn\'t supported by the server';
+    this.errorCode = 406;
+    this.moreInfo = 'Documentation not implemented yet';
+  },
+
+  // 500
+  internalServerError: function(message) {
+    restify.RestError.call(this, {
+      restCode: 'Internal Server Error',
+      statusCode: 500,
+      message: message,
+      constructorOpt: ResponseTemplate.internalServerError
+    });
+    delete this.body;
+    this.statusCode = 500;
+    this.developerMessage = 'internalServerError';
+    this.userMessage = 'An error occured on our side.';
+    this.errorCode = 500;
+    this.moreInfo = 'Documentation not implemented yet';
+  },
+
+  // 501
+  notImplemented: function(message) {
+    restify.RestError.call(this, {
+      restCode: 'Not Implemented',
+      statusCode: 501,
+      message: message,
+      constructorOpt: ResponseTemplate.notFound
+    });
+    delete this.body;
+    this.statusCode = 501;
+    this.developerMessage = 'notImplemented';
+    this.userMessage = 'The action you\'ve tried isn\'t yet implemented.';
+    this.errorCode = 501;
+    this.moreInfo = 'Documentation not implemented yet';
+  }
+};
+
+// Inherit from restify errors
+for (var key in ResponseTemplate) {
+   if (ResponseTemplate.hasOwnProperty(key) && key != 'ok') {
+      var obj = ResponseTemplate[key];
+      util.inherits(obj, restify.RestError);
+   }
+}
+
+module.exports = ResponseTemplate;
